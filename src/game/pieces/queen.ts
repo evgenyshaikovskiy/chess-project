22768ef163192597295445e1573ef03d913f9bb4
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2023-01-08 22:51:27 +0300

    add queen movement

--- src/game/pieces/queen.ts
+++ src/game/pieces/queen.ts
@@ -12,7 +12,113 @@ export class Queen extends Piece {
     super(position, PieceType.QUEEN, color, possibleMoves);
   }
 
-  public updatePossibleMoves(positions: Position[]): void {}
+  public updatePossibleMoves(positions: Position[]): void {
+    this.possibleMoves = [];
 
-  public moveTo(position: Position): void {}
+    if (this.position.y !== 9) {
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          0,
+          1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          1,
+          1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          -1,
+          1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+    }
+
+    if (this.position.y !== 0) {
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          0,
+          -1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          1,
+          -1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          -1,
+          -1,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+    }
+
+    if (this.position.x !== 0) {
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          -1,
+          0,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+    }   
+
+    if (this.position.x !== 9) {
+      this.possibleMoves.push(
+        ...Piece.findMoves(
+          this.position.x,
+          this.position.y,
+          1,
+          0,
+          positions,
+          this.color,
+          (x: number, y: number) => x + 10 * y
+        )
+      );
+    }
+  }
+
+  public moveTo(position: Position): void {
+    position.placePiece(this);
+  }
 }
