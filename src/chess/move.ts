6adc49189251665d84b92d323d1d6e9b1ce5b776
evgenyshaikovskiy <evgenyshaikovskiy@gmail.com>
2023-01-31 12:23:19 +0300

    add input for move exchanging

--- src/chess/move.ts
+++ src/chess/move.ts
@@ -3,17 +3,15 @@ import { Piece } from "./piece";
 export class Move {
   public source: string;
   public destination: string;
-  public pieceShorthandKey: string;
   public piecePicturePath: string;
 
   constructor(source: string, destination: string, piece: Piece) {
     this.source = source;
     this.destination = destination;
-    this.pieceShorthandKey = piece.type;
     this.piecePicturePath = piece.image;
   }
 
   public get interpretation(): string {
-    return `${this.source}-${this.pieceShorthandKey}-${this.destination}`;
+    return `${this.source}-${this.destination}`;
   }
 }
